351e69ba7ec0bea452822f3bddf965606cb47036
ShreyDhyani <shreydhyani@Shreys-MacBook-Air.local>
2024-12-30 19:08:29 +0530

    feat: Added accordian component from shadcn

--- packages/locoui-react/src/index.ts
+++ packages/locoui-react/src/index.ts
@@ -1,3 +1,5 @@
 // Export Components here
 export { Separator } from "./separator/separator.tsx"
+export { Accordion, AccordionItem, AccordionTrigger, AccordionContent } from "./accordian/accordian.tsx"
+
 export { cn } from "./utils/cn.ts"
