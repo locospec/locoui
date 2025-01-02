8796c86d54cc9bfac0b62401bf271021e2a92814
ShreyDhyani <shrey@betalectic.com>
2025-01-02 16:53:09 +0530

    feat: Added trigger value

--- scripts/changeset/get-git-logs.js
+++
@@ -1,8 +0,0 @@
-import { execSync } from 'child_process';
-// import * as fs from "fs";
-// import * as path from "path";
-
-
-const output = execSync('git fetch --tag && git tag', { encoding: 'utf-8' });
-
-console.log(">>>> output",output)
