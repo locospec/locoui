0038bf05aa897e869d24231f1b951b91e0b2432d
ShreyDhyani <shrey@betalectic.com>
2025-01-03 11:26:48 +0530

    feat: Updated and adde dtype saftey for utils code

--- scripts/changeset/utils.js
+++ scripts/changeset/utils.js
@@ -1,5 +1,12 @@
 import * as path from "path";
 
+/**
+ * Get the version of a specified package from the ${repo_name} packages directory from inputs
+ * and validae for presence for each input
+ *
+ * @param void
+ * @returns {{repo_name:string, packageName:string, version_increment:string}} Repo name, package name and version increment.
+ */
 const process_and_validate = () => {
   const repo_name = process.argv[2]; // Expecting repo name to be passed as a command-line argument
   const packageName = process.argv[3]; // Expecting package name to be passed as a command-line argument
@@ -21,7 +28,13 @@ const process_and_validate = () => {
   return { repo_name, packageName, version_increment };
 };
 
-const extract_and_return_file_path_and_name = (output: string) => {
+/**
+ * Get the version of a specified package from the ${repo_name} packages directory.
+ *
+ * @param {string} output - The response of create empty changeset command.
+ * @returns {{changesetFilePath: string, changesetFile: string}} File path and name.
+ */
+const extract_and_return_file_path_and_name = (output) => {
   const changesetPathMatch = output.match(/\.changeset\/([a-z-]+\.md)/);
 
   if (!changesetPathMatch || !changesetPathMatch[1]) {
