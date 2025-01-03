8d007e17b3ad9ffa47379b21c0acb17f4dd5da57
ShreyDhyani <shrey@betalectic.com>
2025-01-03 17:02:36 +0530

    feat: Modified styles for button component

--- packages/locoui-react/src/button/button.tsx
+++ packages/locoui-react/src/button/button.tsx
@@ -1,7 +1,6 @@
 import * as React from "react";
 import { Slot } from "@radix-ui/react-slot";
 import { cva, type VariantProps } from "class-variance-authority";
-
 import { cn } from "../utils/cn.ts";
 
 const buttonVariants = cva(
@@ -10,7 +9,7 @@ const buttonVariants = cva(
     variants: {
       variant: {
         default:
-          "bg-primary text-primary-foreground shadow hover:bg-primary/90",
+          "bg-primary text-primary-foreground shadow hover:bg-primary/80",
         destructive:
           "bg-destructive text-destructive-foreground shadow-sm hover:bg-destructive/90",
         outline:
