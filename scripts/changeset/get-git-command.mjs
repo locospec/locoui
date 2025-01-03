987336ad6147c8ac03c2da0c07dd5f43608dfef4
ShreyDhyani <shrey@betalectic.com>
2025-01-03 17:30:22 +0530

    feat: Added console.logs

--- scripts/changeset/get-git-command.mjs
+++ scripts/changeset/get-git-command.mjs
@@ -49,9 +49,11 @@ export function getGitLogs(repo_name, packageName) {
   }
 
   const gitLogCommand = `git log --oneline @${repo_name}/${packageName}@${version}..HEAD -- ./packages/${repo_name}-${packageName}`;
+  console.log(">>> GIT COMMAND >>>", gitLogCommand);
   try {
     execSync("git fetch --tag");
     const output = execSync(gitLogCommand, { encoding: 'utf-8' });
+    console.log(">>>>> output output output", output);
     return output;
   } catch (err) {
     throw new Error(`Failed to execute git log command: ${err}`);
