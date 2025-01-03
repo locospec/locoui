6eefa9f971c9f7701a55b669be48194cb750272f
ShreyDhyani <shrey@betalectic.com>
2025-01-03 17:07:44 +0530

    feat: Modified code for separator component

--- packages/locoui-react/src/separator/separator.tsx
+++ packages/locoui-react/src/separator/separator.tsx
@@ -1,9 +1,7 @@
-"use client"
-
-import * as React from "react"
-import * as SeparatorPrimitive from "@radix-ui/react-separator"
-
-import { cn } from "../utils/cn.ts"
+"use client";
+import * as React from "react";
+import * as SeparatorPrimitive from "@radix-ui/react-separator";
+import { cn } from "../utils/cn.ts";
 
 const Separator = React.forwardRef<
   React.ElementRef<typeof SeparatorPrimitive.Root>,
@@ -11,21 +9,21 @@ const Separator = React.forwardRef<
 >(
   (
     { className = "", orientation = "horizontal", decorative = true, ...props },
-    ref
+    ref,
   ) => (
     <SeparatorPrimitive.Root
       ref={ref}
       decorative={decorative}
       orientation={orientation}
       className={cn(
-        "shrink-0 bg-border",
+        "bg-border shrink-0",
         orientation === "horizontal" ? "h-[1px] w-full" : "h-full w-[1px]",
-        className
+        className,
       )}
       {...props}
     />
-  )
-)
-Separator.displayName = SeparatorPrimitive.Root.displayName
+  ),
+);
+Separator.displayName = SeparatorPrimitive.Root.displayName;
 
-export { Separator }
+export { Separator };
